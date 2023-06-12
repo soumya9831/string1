adfbdb46522e6040a72b32f9e6fbe53e8c5b1822
soumya9831 <136292182+soumya9831@users.noreply.github.com>
2023-06-12 11:02:45 +0530

    Add files via upload

---
+++ uppercase.java
@@ -0,0 +1,14 @@
+public class uppercase {
+    public static void main(String[] args) {
+       String str ="pw skills java";
+        System.out.println(str);
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.length());
+        System.out.println(str.charAt(1));
+        System.out.println(str.substring(3, 9));
+        System.out.println(str.indexOf("s"));  
+        System.out.println(str.lastIndexOf("a"));
+    }
+    
+}
